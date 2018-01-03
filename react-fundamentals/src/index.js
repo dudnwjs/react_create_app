272d97326b8666c71d3b93cd880eb481c2a49434
dunpeal <hjkim86@sptek.co.kr>
2018-01-03 17:46:20 +0900

    add react-fundamentals

---
+++ react-fundamentals/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+ReactDom.render(<App/>, rootElement);
+
+if(module.hot) {
+    module.hot.accept();
+}
